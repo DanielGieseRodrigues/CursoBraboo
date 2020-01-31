f3e28242cd17a2016c33e5fcd2182f70aeaf2a7e
Marcelo Bernart <marcelo.bernart@gmail.com>
2020-01-31 16:47:50 -0300

    .

--- GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
+++ GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
@@ -10,7 +10,7 @@ namespace GitCapirotescoTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("o curso é MUITO BRABO!");
+            Console.WriteLine("o curso é MUITO BRABO! e eu amo teu pai : by = Celo");
         }
     }
 }
