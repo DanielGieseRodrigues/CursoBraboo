27be626839bff924cabdcbd16b48aa63e8a5074c
Daniel Rodrigues <daniel.giese.rodrigues@gmail.com>
2020-01-31 15:59:05 -0300

    Segunda modificacao da branch

--- GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
+++ GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
@@ -10,7 +10,7 @@ namespace GitCapirotescoTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("o curso é MUITO BRABO!!!!!!!!!!!! E EU AMO O MARCELO ");
+            Console.WriteLine("o curso é MUITO BRABO!!!!!!!!!!!! E EU AMO O MARCELO.");
         }
     }
 }
